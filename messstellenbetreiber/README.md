c1b0e5faecaf3295aa67a4ca4d67c01fb2f146ee
chibbi <chehab.djibril@gmail.com>
2023-10-17 16:07:12 +0200

    Working on db connection

--- messstellenbetreiber/README.md
+++ messstellenbetreiber/README.md
@@ -2,7 +2,7 @@ messtellenbetreiber
 
 Datenbank:
 Tabellen:
-- Kunde Stromzähler Relationstabelle:
+- Kunde_Stromzähler Relationstabelle:
     - KundenId
     - StromzählerId
 - Kunde Datentabelle:
@@ -12,14 +12,20 @@ Tabellen:
     - Hausnummerzusatz
     - Postleitzahl
     - Stadtname
-- Stromzähler Strom Datentabelle:
+- Stromzähler_Strom Datentabelle:
     - StromzählerId
     - Stromverbrauch_gesamt # in Watt
     - Stromverbrauch_momentan (if possible) # in Watt
     - Uhrzeit # in sekunden seit 1970
-- Stromzähler Wartung Datentabelle:
+- Stromzähler_Wartung Datentabelle:
     - StromzählerId
     - Einbau_datum # in sekunden seit 1970
     - letzte_manuelle_Ablese_datum # in sekunden seit 1970
     - letzte_Eichung_datum # in sekunden seit 1970
     - nächste_Wartung_datum # in sekunden seit 1970
+
+SQL_Befehle:
+Read stromverbrauch per Year:
+"""
+SELECT KundenId, sz.Stromverbrauch_gesamt, SUM(sz.Stromverbrauch_momentan) AS test FROM Stromzähler_Strom as sz INNER JOIN Kunde as k ON sz.StromzählerId=k.StromzählerId;
+"""
--- messstellenbetreiber/msbDatabase/router.js
+++ messstellenbetreiber/msbDatabase/router.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const dbConnector = require("./dbConnector");
 const router = express.Router();
-const endpoints = "/api/v1/";
+const api_location = "/api/v1/";
+const endpoints = {
+    "stromverbrauch": api_location + "stromverbrauch/",
+    "current_consumption": endpoints["stromverbrauch"] + "current",
+};
 const db_type = true;
 const sqlite_file = "msb.db";
 const dbConnection = new dbConnector(db_type, sqlite_file);
@@ -13,15 +17,19 @@ router.use((request, response, next) => {
     response.send("Nö du hast kein access");
 });
 
-router.get(endpoints + "historical_data", (request, response) => {
+router.get(endpoints["stromverbrauch"], (request, response) => {
     response.send('Soll die Stromverbrauchsdaten eines Kunden zurückgeben');
 });
 
-router.get(endpoints + "historical_data" + "/:year", (request, response) => {
+router.get(endpoints["current_consumption"], (request, response) => {
+    response.send('Soll die Stromverbrauchsdaten eines Kunden zurückgeben');
+});
+
+router.get(endpoints["stromverbrauch"] + "/:year", (request, response) => {
     response.send("Soll die Stromverbrauchsdaten des Jahresponse " + request.params.year + " eines Kunden zurückgeben");
 });
 
-router.get(endpoints + "historical_data" + "/:year/:month", (request, response) => {
+router.get(endpoints["stromverbrauch"] + "/:year/:month", (request, response) => {
     response.send("Soll die Stromverbrauchsdaten des Jahresponse " + request.params.year + " und Monats " + request.params.month + " eines Kunden zurückgeben");
 });
 module.exports = router
