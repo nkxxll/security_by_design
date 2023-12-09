9d7e6d16e7d9a86431fcf445f0aa25e63c6020e8
nkirschall <niklas.kirschall@eviden.net>
2023-12-09 22:02:51 +0100

    feat(kundenportal): added logging in init file

---
+++ kundenportal/kundenportal/kundenportal/__init__.py
@@ -0,0 +1,15 @@
+from sys import stderr
+from logging import FileHandler, getLogger, StreamHandler, DEBUG, Formatter
+LOGGER = getLogger(__name__)
+LOGGER.setLevel(DEBUG)
+formatter = Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+stream_handler = StreamHandler(stderr)
+stream_handler.setLevel(DEBUG)
+stream_handler.setFormatter(formatter)
+file_handler = FileHandler("kundenportal.log")
+file_handler.setLevel(DEBUG)
+file_handler.setFormatter(formatter)
+LOGGER.addHandler(stream_handler)
+LOGGER.addHandler(file_handler)
+
+LOGGER.info("Starting kundenportal")
